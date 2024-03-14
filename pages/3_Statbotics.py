406b1c74c3b6c1af2d7fc708b339929e75b19929
Akash Maiti <akashmaiti@akashs-air.eanes-ad.eanesisd.net>
2024-03-14 10:45:17 -0500

    Testing pt 5

--- pages/3_Statbotics.py
+++ pages/3_Statbotics.py
@@ -10,7 +10,7 @@ print(pit_data_path)
 pit_data = pd.read_csv(pit_data_path)
 team_number = st.selectbox("Select Team", pit_data["Team Number"].unique(), format_func=lambda x: f"{x}")
 if team_number:
-    st.markdown("[To see more information about this team, click here](https://www.statbotics.io/team/" + str(team_number))
+    st.markdown("[To see more information about this team, click here](https://www.statbotics.io/team/" + str(team_number) + ")")
     st.markdown("[To see more about the Fort Worth Event, click here](https://www.statbotics.io/event/2024txfor)")
 
 
