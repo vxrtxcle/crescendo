75574fdd61173d8126ded7a9f4275f40a0c15f54
Akash Maiti <akashmaiti@Akashs-MacBook-Air.local>
2024-03-28 23:06:07 -0500

    jfhs

--- pages/2_RLD_Data.py
+++ pages/2_RLD_Data.py
@@ -12,7 +12,7 @@ url = "https://www.thebluealliance.com/api/v3/event/2024txfor/teams/keys"
 headers = {'X-TBA-Auth-Key': tba_key}
 response = requests.get(url, headers)
 req = response.json()
-print(req)
+os.write(1, f'{req}'.encode())
 teams = []
 for x in req:
     y = x.replace('frc','')
