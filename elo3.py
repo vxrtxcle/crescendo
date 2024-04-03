8c196978ec3ee2d2f13175655a3d296352e48132
Akash Maiti <akashmaiti@Akashs-MacBook-Air.local>
2024-04-03 17:36:40 -0500

    Fixing RLD Data

--- elo3.py
+++ elo3.py
@@ -22,8 +22,6 @@ class node:
             self.list.append(x)
     def add_rating(self, item):
         self.rating_array.append(item)
-    '''
-'''
     def remove_duplicates(self):
         unique_list = []
         seen_ids = set()
@@ -44,17 +42,18 @@ class node:
         team = self.team
         with open('match2.json') as f:
             x = json.load(f)
-            for y in range(1,6):
+            matches_obj = x['matches']
+            keys = matches_obj.keys()
+            for y in range(1, len(keys)+1):
                 z = "match" + str(y)
                 if int(x['matches'][z]["t1"]) == team:
-
                     if int(x['matches'][z]["r1"]) == 1:
                         continue
                     elif int(x['matches'][z]["r1"]) == 2:
                         if int(x['matches'][z]["r2"]) == 3:
                             self.list.append(node(x['matches'][z]["t3"]))
                         else:
-                            self.list.append(node(x['matches'][z]["t1"]))
+                            self.list.append(node(x['matches'][z]["t2"]))
                     else:
                         self.list.append(node(x['matches'][z]["t2"]))
                         self.list.append(node(x['matches'][z]["t3"]))
@@ -83,49 +82,6 @@ class node:
                 else:
                     continue
             self.check_list()
-    def generate_other_list(self):
-        team = self.team
-        with open('match2.json') as f:
-            x = json.load(f)
-            for y in range(1, 6):
-                z = "match" + str(y)
-                if int(x['matches'][z]["t1"]) == team:
-
-                    if int(x['matches'][z]["r1"]) == 3:
-                        continue
-                    elif int(x['matches'][z]["r1"]) == 2:
-                        if int(x['matches'][z]["r2"]) == 1:
-                            self.list.append(x['matches'][z]["t3"])
-                        else:
-                            self.list.append(x['matches'][z]["t1"])
-                    else:
-                        self.list.append(x['matches'][z]["t2"])
-                        self.list.append(x['matches'][z]["t3"])
-                elif int(x['matches'][z]["t2"]) == team:
-                    if int(x['matches'][z]["r2"]) == 1:
-                        continue
-                    elif int(x['matches'][z]["r2"]) == 2:
-                        if int(x['matches'][z]["r3"]) == 3:
-                            self.list.append(x['matches'][z]["t1"])
-                        else:
-                            self.list.append(x['matches'][z]["t3"])
-                    else:
-                        self.list.append(x['matches'][z]["t1"])
-                        self.list.append(x['matches'][z]["t3"])
-                elif int(x['matches'][z]["t3"]) == team:
-                    if int(x['matches'][z]["r3"]) == 1:
-                        continue
-                    elif int(x['matches'][z]["r3"]) == 2:
-                        if int(x['matches'][z]["r2"]) == 3:
-                            self.list.append(x['matches'][z]["t1"])
-                        else:
-                            self.list.append(x['matches'][z]["t2"])
-                    else:
-                        self.list.append(x['matches'][z]["t1"])
-                        self.list.append(x['matches'][z]["t2"])
-                else:
-                    continue
-            self.check_list()
 
 
 def find_teams_worse(teams, team):
@@ -136,11 +92,13 @@ def find_teams_worse(teams, team):
     return list
 
 def generate_better_than_list():
-
     node_list = []
     with open("match2.json", "r") as f:
         x = json.load(f)
-        for y in range(1,6):
+        matches_obj = x['matches']
+        keys = matches_obj.keys()
+        #print(len(keys))
+        for y in range(1, len(keys)+1):
             b = "match" + str(y)
             for z in range(1,4):
                 c = "t" + str(z)
@@ -182,7 +140,6 @@ def find_node_by_team(nodes, target_team):
 
 def weighed_graph():
     y = generate_better_than_list()
-    print(y)
     G = nx.DiGraph()
     seen_teams = []
     seen_nodes = []
@@ -217,20 +174,14 @@ def weighed_graph():
                 edge_list.append([int(node.team), int(outperformed_team), 1])
     for x in seen_nodes:
         G.add_node(x)
-    print(edge_list)
-    print("x")
+    #print("x")
     for edge in edge_list:
         if G.has_edge(find_node_by_team(seen_nodes,edge[0]), find_node_by_team(seen_nodes,edge[1])):
             continue
-            print("z")
         else:
-            print("y")
             if check_sublist_presence(edge_list, edge[0], edge[1]):
-                print("z")
                 weight = find_third_value(edge_list,edge[0],edge[1])
-                print(weight)
                 other_weight = find_third_value(edge_list,edge[1],edge[0])
-                print(other_weight)
                 total = find_third_value(edge_list, edge[0],edge[1]) + find_third_value(edge_list, edge[1],edge[0])
                 print("Node 1: " + str(edge[0]) + ", Node 2: " + str(edge[1]) + ", Weight: " + str(weight/total))
                 print(edge_list)
@@ -238,7 +189,6 @@ def weighed_graph():
                 G.add_edge(find_node_by_team(seen_nodes,edge[1]),find_node_by_team(seen_nodes,edge[0]),weight=other_weight/total)
 
 # Example usage (replace with your actual data)
-    print(G.edges(data=True))
     remove = []
     for x in G.edges:
         if x[0] == x[1]:
@@ -271,13 +221,16 @@ def weighed_graph():
             print("Node 1: " + str(u.team))
             print("Node 2: " + str(v.team))
             seen.append(currTeam.team)
+            #print(seen)
             if v.team == B:
                 return {'prob': G[u][v]['weight'], 'dist': 1}
             if u.team in seen and v.team in seen:
                 continue
             temp = R(A, B, v, G, seen_nodes, seen, found)
-            if v.team == B:
-                found.append({'prob': temp['prob'] * G[u][v]['weight'], 'dist': temp['dist'] + 1})
+            print(v.team)
+            print(temp)
+            if G.has_edge(v, find_node_by_team(seen_nodes, B)):
+                found.append({'prob': temp['prob'] * G[u][v]['weight'], 'dist': temp['dist'] + 2})
             print(found)
         lowest = None
         if len(found) == 0:
@@ -287,17 +240,18 @@ def weighed_graph():
         lowest = found[0]['dist']
         edge_weight = found[0]['prob']
         for x in range(len(found)):
+            # maximize edge weight
             temp = found[x]['dist']
             temp2 = found[x]['prob']
             if temp <= lowest:
                 lowest = temp
                 edge_weight = found[x]['prob']
-            print(edge_weight)
-            print(temp2)
+            #print(edge_weight)
+            #print(temp2)
             if temp2 > edge_weight:
                 edge_weight = temp2
-                print(str(edge_weight) + " Edge Weight!!")
-        return {'prob': edge_weight, 'dist': found}
+                #print(str(edge_weight) + " Edge Weight!!")
+        return {'prob': edge_weight, 'dist': lowest}
 
 
     #print(G.has_edge(find_node_by_team(seen_nodes, 3310),find_node_by_team(seen_nodes, 3743)))
@@ -305,8 +259,6 @@ def weighed_graph():
         #print(x)
     A = input("What team are you comparing 1st? ")
     B = input("What team are you comparing 2nd? ")
-    print(int(A))
-    print(int(B))
     currTeam = find_node_by_team(seen_nodes, int(A))
 
     '''for x,y in G.edges:
--- pages/2_RLD_Data.py
+++ pages/2_RLD_Data.py
@@ -19,12 +19,13 @@ for x in req:
     teams.append(int(y))
 teams.sort()
 options = st.multiselect("What team are you selecting?", teams)
+
 edict = {'Match Number': [], 'Auto Speaker': [], 'Auto Speaker Miss': [], 'Tele Speaker': [], "Tele Speaker Miss": [], 'Tele Amp': [], 'Tele Amp Miss': [], 'Drops': [], 'Endgame Stage': [], 'DC': []}
 if options:
     for x in options:
         edict = {'Match Number': [], 'Auto Speaker': [], 'Auto Speaker Miss': [], 'Tele Speaker': [],
                  "Tele Speaker Miss": [], 'Tele Amp': [], 'Tele Amp Miss': [], 'Drops': [], 'Endgame Stage': [],
-                 'DC': []}
+                 'DC': [], 'Avg': 0, 'Min': 0, 'Max': 0}
         with open("Fort Worth.json", 'r') as f:
             g = json.load(f)
             for y in g:
@@ -57,8 +58,48 @@ if options:
                     else:
                         edict['Tele Amp Miss'].append(0)
                     edict['Drops'].append(y['Drops'])
-                    edict['Endgame Stage'].append(y['endgame_stage_actions'])
+                    if y['endgame_stage_actions'] == 'None':
+                        count = (y["endgame_trap_scored"] + y['tele_trap_scored']) * 5
+                        edict['Endgame Stage'].append(count)
+                    elif y['endgame_stage_actions'] == 'Onstage':
+                        count = (y["endgame_trap_scored"] + y['tele_trap_scored']) * 5
+                        edict['Endgame Stage'].append(count + 3)
+                    elif y['endgame_stage_actions'] == 'Onstage Spotlit':
+                        count = (y["endgame_trap_scored"] + y['tele_trap_scored']) * 5
+                        edict['Endgame Stage'].append(count + 4)
+                    elif y['endgame_stage_actions'] == 'Park':
+                        count = (y["endgame_trap_scored"] + y['tele_trap_scored']) * 5
+                        edict['Endgame Stage'].append(count + 1)
+                    if y['disconnect'] == 'Yes':
+                        edict['DC'].append(1)
+                    else:
+                        edict['DC'].append(0)
+
+            for x in range(len(edict.keys())-3):
+                total = 0
+                klist = list(edict.keys())
+                for y in edict[klist[x]]:
+                    if y == 'Match Number':
+                        continue
+                    else:
+                        min = 9999
+                        max = -9999
+                        if y < min:
+                            min = y
+                        elif y > max:
+                            max = y
+                        total += y
+                avg = total / len(edict[klist[x]])
+                edict['Avg'] = avg
+                edict['Max'] = max
+                edict['Min'] = min
+            for y in g:
+                if int(y['team_#']) == int(x):
+                    edict['Endgame Stage'] = []
+                    edict['DC'] = []
                     edict['DC'].append(y['disconnect'])
+                    edict['Endgame Stage'].append(y['endgame_stage_actions'])
+
         st.subheader("Team #" + str(x))
         match_table = pd.DataFrame.from_dict(edict)
         st.table(match_table)
